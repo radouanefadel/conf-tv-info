7f34c2c256a50dc18554bf0f356dd3b0a2bd1003
radouanefadel <radouane.ai@gmail.com>
2021-12-03 02:37:44 +0100

    Add header component
    Related to #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Layouts } from './ui';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		Layouts,
 	],
 	imports: [
 		BrowserModule,
---
+++ src/app/ui/index.ts
@@ -0,0 +1,5 @@
+import { HeaderComponent } from './layouts';
+
+export const Layouts: Array<any> =  [
+	HeaderComponent,
+];
